e1b63d9d54bd782a4bc8fd2e6e0ee17e4a22835e
Ben Lesh <blesh@netflix.com>
2015-02-06 17:01:02 -0800

    add initial multiplex implementation

--- dist/rx-socket-subject.js
+++ dist/rx-socket-subject.js
@@ -172,7 +172,88 @@
     }
 
     $$RxSocketSubject$create$$RxSocketSubject.prototype = $$utils$$extend(Object.create($$RxSocketSubject$create$$AnonymousSubject.prototype), {
-        constructor: $$RxSocketSubject$create$$RxSocketSubject
+        constructor: $$RxSocketSubject$create$$RxSocketSubject,
+
+        /**
+            Creates a function that will create a child observable from the RxSocketSubject
+
+            Usage is as follows:
+
+                        // set up an RxSocketSubject
+                      var endpoints = Observable.just('ws://mysocketserver');
+                        var socket = RxSocketSubject.create(endpoints);
+
+                        // create a n observable factory
+                        var fromTickerRequest = socket.multiplex(function(request) {
+                            return function(data) {
+                                return data.requestId === request.requestId;
+                            }
+                        });
+
+                        // create a new observable of multiplexed ticker data
+                        var subscriptionData = { requestId: 1, subscribeTo: 'NFLX' };
+                        var unsubscriptionData = { requestId: 1, unsubscribeFrom: 'NFLX' };
+                        var netflixTickerData = fromTickerRequest(subscriptionData, unsubscriptionData);
+
+                        // subscribe to the ticker data
+                        netflixTickerData.subscribe(function(responseData) {
+                            console.log(responseData);
+                        });
+
+            @method multiplex
+            @param {Function} responseFilter a predicate to filter response messages that belong to the given multiplexed stream.
+            @param {Object} [options] an optional hash of additional configuration options for the multiplexer. This
+                includes configuration for serializing outbound messages and deserializing inbound messages.
+
+                Defaults are as follows:
+
+                            {
+                                serializer: function(data) {
+                                    return JSON.stringify(data);
+                                },
+                                deserializer: function(e) {
+                                    return JSON.parse(e.data);
+                                }
+                            }
+
+            @return {Function} a function to create an multiplexed socket observable from the current socket. This
+                function accepts arguments for `subscriptionData` and `unsubscriptionData`.
+        */
+        multiplex: function (responseFilter, options) {
+            var socket = this;
+            
+            var config = {
+                serializer: function(data) {
+                    return JSON.stringify(data);
+                },
+                deserializer: function(e) {
+                    return JSON.parse(e.data);
+                }
+            };
+
+            if(options) {
+                $$utils$$extend(config, options);
+            }
+
+            return function multiplex(subscriptionData, unsubscriptionData) {
+                return $$RxSocketSubject$create$$Observable.create(function(obs) {
+                    var message = config.serializer(subscriptionData);
+                    socket.onNext(message);
+                    var disposable = socket.map(config.deserializer).
+                        filter(responseFilter(subscriptionData)).
+                        subscribe(obs);
+
+                    var multiplexUnsub = function() {
+                        socket.onNext(config.serializer(unsubscriptionData));
+                    };
+
+                    return function() {
+                        multiplexUnsub();
+                        disposable.dispose();
+                    }
+                });
+            }
+        }
     });
 
     $$RxSocketSubject$create$$RxSocketSubject.create = $$RxSocketSubject$create$$create;
