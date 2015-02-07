6204ccea494c35fbeeae94b9dc4e5103eb34dc90
Ben Lesh <blesh@netflix.com>
2015-02-06 17:07:49 -0800

    rename create

---
+++ dist/amd/RxSocketSubject/rx-socket-subject.js
@@ -0,0 +1,251 @@
+define(
+    "RxSocketSubject/rx-socket-subject",
+    ["./utils", "exports"],
+    function(RxSocketSubject$utils$$, __exports__) {
+        "use strict";
+
+        function __es6_export__(name, value) {
+            __exports__[name] = value;
+        }
+
+        var extend;
+        extend = RxSocketSubject$utils$$["extend"];
+
+        var Subject = Rx.Subject;
+        var Observable = Rx.Observable;
+        var Observer = Rx.Observer;
+        var fromWebSocket = Rx.DOM.fromWebSocket;
+        var AnonymousSubject = Rx.AnonymousSubject;
+
+        function RxSocketSubject(config) {
+            var connections = this.connections = config.connections;
+            var openObserver = this.openObserver = config.openObserver;
+            var errorObserver = this.errorObserver = config.errorObserver;
+            var closingObserver = this.closingObserver = config.closingObserver;
+
+            var observer = new Subject();
+            var toSocket = new Subject();
+            var msgBuffer = [];
+            var isOpen = false;
+
+            var socketOpen = function(e) {
+                isOpen = true;
+
+                if(openObserver) {
+                    openObserver.onNext(e);
+                }
+
+                while(msgBuffer.length > 0) {
+                    var msg = msgBuffer.shift();
+                    toSocket.onNext(msg);
+                }
+            };
+
+            var socketClosed = function() {
+                isOpen = false;
+            };
+
+          // subscribe to outward facing observer
+          // and buffer messages if necessary
+          observer.subscribe(function(msg) {
+                if(isOpen) {
+                    toSocket.onNext(msg);
+                } else {
+                    msgBuffer.push(msg);
+                }
+            }, 
+            toSocket.onError.bind(toSocket), 
+            toSocket.onCompleted.bind(toSocket));
+
+            var i = 0;
+            var innerObservable;
+            var hasInnerObservable = false;
+            var getInnerObservable = function(){
+                if(!hasInnerObservable) {
+                    innerObservable = Observable.create(function(o) {
+                        var dy = new Rx.SerialDisposable();
+
+                        var disposable = new Rx.CompositeDisposable(dy,
+                                connections.map(function(conn) {
+                                    var url;
+                                    var protocol = null;
+                                    if(typeof conn === 'string') {
+                                        url = conn;
+                                    }
+                                    else if(conn && conn.url) {
+                                        url = conn.url;
+                                        protocol = conn.protocol;
+                                    }
+
+                                    return fromWebSocket(url, protocol, Observer.create(function(e) {
+                                        socketOpen(e);
+                                    }), closingObserver);
+                                }).subscribe(function(socket) {
+                                    if(dy && !dy.isDisposed) {
+                                        dy.setDisposable(new Rx.CompositeDisposable(
+                                      socket.subscribe(function(e) {
+                                                o.onNext(e);
+                                            }, function(err) {
+                                                if(errorObserver) {
+                                                    errorObserver.onNext(err);
+                                                }
+                                                socketClosed();
+                                                o.onError(err);
+                                            }, function() {
+                                                socketClosed();
+                                                o.onCompleted();
+                                            }),
+
+                                            toSocket.subscribe(socket)
+                                      ));
+                                    }
+                                },
+                                o.onError.bind(o))
+                        );
+
+                      return function(){
+                        socketClosed();
+                        if(disposable && !disposable.isDisposed) {
+                            disposable.dispose();
+                          }
+                      };
+                    }).finally(function(){
+                        hasInnerObservable = false;
+                    }).publish().refCount();
+
+                    hasInnerObservable = true;
+                }
+
+                return innerObservable;
+            };
+
+            var observable = Observable.create(function(o) {
+                var disposable = getInnerObservable().subscribe(o);
+                return disposable;
+            });
+
+            AnonymousSubject.call(this, observer, observable);
+        }
+
+        RxSocketSubject.prototype = extend(Object.create(AnonymousSubject.prototype), {
+            constructor: RxSocketSubject,
+
+            /**
+                Creates a function that will create a child observable from the RxSocketSubject
+
+                Usage is as follows:
+
+                            // set up an RxSocketSubject
+                          var endpoints = Observable.just('ws://mysocketserver');
+                            var socket = RxSocketSubject.create(endpoints);
+
+                            // create a n observable factory
+                            var fromTickerRequest = socket.multiplex(function(request) {
+                                return function(data) {
+                                    return data.requestId === request.requestId;
+                                }
+                            });
+
+                            // create a new observable of multiplexed ticker data
+                            var subscriptionData = { requestId: 1, subscribeTo: 'NFLX' };
+                            var unsubscriptionData = { requestId: 1, unsubscribeFrom: 'NFLX' };
+                            var netflixTickerData = fromTickerRequest(subscriptionData, unsubscriptionData);
+
+                            // subscribe to the ticker data
+                            netflixTickerData.subscribe(function(responseData) {
+                                console.log(responseData);
+                            });
+
+                @method multiplex
+                @param {Function} responseFilter a predicate to filter response messages that belong to the given multiplexed stream.
+                @param {Object} [options] an optional hash of additional configuration options for the multiplexer. This
+                    includes configuration for serializing outbound messages and deserializing inbound messages.
+
+                    Defaults are as follows:
+
+                                {
+                                    serializer: function(data) {
+                                        return JSON.stringify(data);
+                                    },
+                                    deserializer: function(e) {
+                                        return JSON.parse(e.data);
+                                    }
+                                }
+
+                @return {Function} a function to create an multiplexed socket observable from the current socket. This
+                    function accepts arguments for `subscriptionData` and `unsubscriptionData`.
+            */
+            multiplex: function (responseFilter, options) {
+                var socket = this;
+
+                var config = {
+                    serializer: function(data) {
+                        return JSON.stringify(data);
+                    },
+                    deserializer: function(e) {
+                        return JSON.parse(e.data);
+                    }
+                };
+
+                if(options) {
+                    extend(config, options);
+                }
+
+                return function multiplex(subscriptionData, unsubscriptionData) {
+                    return Observable.create(function(obs) {
+                        var message = config.serializer(subscriptionData);
+                        socket.onNext(message);
+                        var disposable = socket.map(config.deserializer).
+                            filter(responseFilter(subscriptionData)).
+                            subscribe(obs);
+
+                        var multiplexUnsub = function() {
+                            socket.onNext(config.serializer(unsubscriptionData));
+                        };
+
+                        return function() {
+                            multiplexUnsub();
+                            disposable.dispose();
+                        }
+                    });
+                }
+            }
+        });
+
+        RxSocketSubject.create = create;
+
+        /**
+            Creates a new Socket Subject. The socket subject is an observable of socket message events, as well
+            as an observer of messages to send over the socket with `onNext()`, an a means to close the socket
+            with `onCompleted()` or `onError()`.
+
+            @method create
+            @param connections {Rx.Observable} an observable of connection information, either endpoint URL strings,
+                or objects with `{ url: someUrl, protocol: someProtocol }`.
+            @param openObserver {Rx.Observer} [optional] an observer that will trigger
+                when the underlying socket opens. Will never error or complete.
+            @param errorObserver {Rx.Observer} [optional] an observer that emits errors occurring on the 
+                socket. Will never error or complete.
+            @param closingObserver {Rx.Observer} [optional] an obsesrver that emits when the socket is about to close.
+        */
+        function create(connections, openObserver, errorObserver, closingObserver) {
+            var config;
+            if(connections instanceof Observable) {
+                console.warn('DEPRECATION: RxSocketSubject.create() should be called with a configuration object');
+                config = {
+                    connections: connections,
+                    openObserver: openObserver,
+                    errorObserver: errorObserver,
+                    closingObserver: closingObserver
+                };
+            } else {
+                config = connections;
+            }
+            return new RxSocketSubject(config);
+        }
+
+        __es6_export__("default", RxSocketSubject);
+    }
+);
+
+//# sourceMappingURL=rx-socket-subject.js.map
---
+++ test/RxSocketSubject/rx-socket-subject.js
@@ -0,0 +1,335 @@
+MockSocket.OPEN = 1;
+
+describe('RxSocketSubject.create()', function(){
+	var originalWebSocket;
+	var disposable;
+
+	beforeEach(function(){
+		originalWebSocket = window.WebSocket;
+		window.WebSocket = MockSocket;
+	});
+
+	afterEach(function(){
+		if(disposable) {
+			disposable.dispose();
+		}
+		while(sockets.length) {
+			sockets.shift();
+		}
+		window.WebSocket = originalWebSocket;
+	});
+
+	it('should exist', function(){
+		expect(typeof RxSocketSubject.create).toBe('function');
+	});
+
+	it('should create a Subject', function() {
+		var socketSubject = RxSocketSubject.create(Rx.Observable.just(''));
+		expect(typeof socketSubject.observer !== 'undefined' && typeof socketSubject.observable !== 'undefined').toBe(true);
+	});
+
+	it('should accept an openObserver argument', function(done){
+		var openObserver = Rx.Observer.create(function(e) {
+			done();
+		});
+
+		var socketSubject = RxSocketSubject.create(Rx.Observable.just(''), openObserver);
+
+		disposable = socketSubject.subscribe(function(){});
+
+		sendOpen();
+	});
+
+	it('should pass through connections errors', function(done){
+		var socketSubject = RxSocketSubject.create(Rx.Observable.throw('wat'));
+
+		disposable = socketSubject.subscribe(function(){}, function(err) { 
+			expect(err).toBe('wat');
+			done();
+		});
+	});
+	
+	it('should accept an errorObserver argument', function(done){
+		var called = false;
+
+		var errorObserver = Rx.Observer.create(function(e) {
+			done();
+		});
+
+		var socketSubject = RxSocketSubject.create(Rx.Observable.just(''), undefined, errorObserver);
+
+		disposable = socketSubject.subscribe(function(){});
+
+		sendError('err');
+	});
+
+	describe('the returned subject', function() {
+		it('should create one socket, and close the socket when all subscribed observables are complete', function(){
+			var socketSubject = RxSocketSubject.create(Rx.Observable.just(''));
+			var disp1 = socketSubject.subscribe(function(){});
+			var disp2 = socketSubject.subscribe(function(){});
+
+			expect(sockets.length).toBe(1);
+
+			var socket = sockets[0];
+
+			disp1.dispose();
+			expect(socket.close).not.toHaveBeenCalled();
+
+			disp2.dispose();
+			expect(socket.close).toHaveBeenCalled();
+		});
+
+		it('should create a WebSocket with the passed endpoint on subcription', function(){
+			var socketSubject = RxSocketSubject.create(Rx.Observable.just('ws://test.com'));
+			disposable = socketSubject.subscribe(function(x) {});
+			expect(sockets[0].url).toBe('ws://test.com');
+		});
+
+		it('should provide a stream of messages from the socket and complete on close', function(done){
+			var socketSubject = RxSocketSubject.create(Rx.Observable.just('ws://test.com'));
+			var results = [];
+
+			disposable = socketSubject.subscribe(function(x) {
+				results.push(x);
+			}, function(e){
+				expect(true).toBe(false); //ghetto? yes.
+				done();
+			}, function(){
+				expect(results.map(function(e) {
+					return e.data;
+				})).toEqual(['one', 'two', 'three']);
+				done();
+			});
+			
+			sendMessage('one');
+			sendMessage('two');
+			sendMessage('three');
+			sendClose(1000, 'done');
+		});
+
+		it('should accept a closingObserver that emits when the socket is about to close', function(done) {
+			var socket;
+
+			var closingObserver = Rx.Observer.create(function() {
+				expect(socket.close).not.toHaveBeenCalled();
+				done();
+			});
+
+			var socketSubject = RxSocketSubject.create(Rx.Observable.just(''), null, null, closingObserver);
+
+			var disp = socketSubject.subscribe(function(){});
+
+			socket = sockets[0];
+
+			disp.dispose();
+		});
+
+		describe('onNext', function() {	
+			it('should buffer messages if no socket exists yet', function(){var socket;
+				var socketSubject = RxSocketSubject.create(Rx.Observable.just(''));
+
+				socketSubject.onNext('one');
+				socketSubject.onNext('two');
+				socketSubject.onNext('three');
+
+				disposable = socketSubject.subscribe(function(){});
+				var socket = sockets[0];
+				
+				expect(socket.send.calls.all().length).toBe(0);
+
+				sendOpen();
+
+				expect(socket.send.calls.all()).toEqual([
+					{ object: socket, args: ['one'] },
+					{ object: socket, args: ['two'] },
+					{ object: socket, args: ['three'] }
+				]);
+			});
+
+			it('should buffer messages AGAIN if the underlying socket has been closed because the observable has been disposed', function(){
+				var socketSubject = RxSocketSubject.create(Rx.Observable.just(''));
+
+				var disp1 = socketSubject.subscribe(function(){});
+
+				var socket = sockets[0];
+				
+				expect(socket.send.calls.all().length).toBe(0);
+				sendOpen();
+
+				disp1.dispose();
+
+				socketSubject.onNext('one');
+				socketSubject.onNext('two');
+				socketSubject.onNext('three');
+
+				disposable = socketSubject.subscribe(function(){});
+				
+				// get the socket again, because the dispose killed the other one.
+				socket = sockets[0];
+
+				expect(socket.send.calls.all().length).toBe(0);
+				sendOpen();
+
+				expect(socket.send.calls.all()).toEqual([
+					{ object: socket, args: ['one'] },
+					{ object: socket, args: ['two'] },
+					{ object: socket, args: ['three'] }
+				]);
+			});
+
+			it('should send a message to the socket when it is called and the socket is OPEN', function() {
+				var socketSubject = RxSocketSubject.create(Rx.Observable.just(''));
+				
+				disposable = socketSubject.subscribe(function(){});
+
+				sendOpen();
+
+				socketSubject.onNext('hello');
+
+				var socket = sockets[0];
+
+				expect(socket.send).toHaveBeenCalledWith('hello');
+			});
+		});
+
+	 	describe('onError', function(){
+		  it('should call socket.close(4003, "shazbot") when called with a { code: 4003, message: "shazbot" }', function(){
+				var socketSubject = RxSocketSubject.create(Rx.Observable.just(''));
+
+				disposable = socketSubject.subscribe(function(){});
+
+				var socket = sockets[0];
+
+				socketSubject.onError({ code: 4003, reason: 'shazbot' });
+
+				expect(socket.close).toHaveBeenCalledWith(4003, 'shazbot');
+			});
+		});
+
+	 	describe('onCompleted', function() {
+			it('should call socket.close() when called with no arguments', function(){
+				var socketSubject = RxSocketSubject.create(Rx.Observable.just(''), undefined, undefined, undefined);
+
+				disposable = socketSubject.subscribe(function(){});
+
+				var socket = sockets[0];
+
+				socketSubject.onCompleted();
+
+				expect(socket.close).toHaveBeenCalledWith(1000, '');
+			});
+		});
+
+		describe('when subscribe or subscribe is called', function(){
+			it('should create a socket', function(){
+				var socketSubject = RxSocketSubject.create(Rx.Observable.just(''));
+
+				expect(sockets.length).toBe(0);
+
+				disposable = socketSubject.subscribe(function(){});
+
+				expect(sockets.length).toBe(1);
+			});
+
+			describe('more than once', function() {
+				it('should only create one socket', function(){
+					var socketSubject = RxSocketSubject.create(Rx.Observable.just(''));
+
+					disposable = new Rx.CompositeDisposable(
+						socketSubject.subscribe(function(){}),
+						socketSubject.subscribe(function(){})
+					);
+
+					expect(sockets.length).toBe(1);
+				});
+			});
+	 	});
+	});
+});
+
+var sockets = [];
+
+function sendOpen() {
+	sockets.forEach(function(socket) {
+		socket.readyState = 1;
+		socket.dispatchEvent(createEvent(socket, 'open'));
+	});
+}
+
+
+function sendMessage(data) {
+	sockets.forEach(function(socket) {
+		socket.dispatchEvent(createEvent(socket, 'message', { data: data }));
+	});
+}
+
+
+function sendClose(code, reason, wasClean) {
+	wasClean = (arguments.length === 3) ? wasClean : true;
+	sockets.forEach(function(socket) {
+		socket.readyState = 3;
+		socket.dispatchEvent(createEvent(socket, 'close', { code: code, reason: reason, wasClean: wasClean }));
+	});
+}
+
+function sendError(err) {
+	sockets.forEach(function(socket) {
+		socket.readyState = 3;
+		socket.dispatchEvent(createEvent(socket, 'error'));
+	});
+}
+
+function MockSocket(url, prototype) {
+	this.url = url;
+	this.prototype = prototype;
+
+	sockets.push(this);
+
+ 	var handlers = {};
+
+	this.addEventListener = jasmine.createSpy('addEventListener').and.callFake(function(name, handler) {
+		handlers[name] = handlers[name] || [];
+		handlers[name].push(handler);
+	});
+
+	this.removeEventListener = jasmine.createSpy('removeEventListener').and.callFake(function(name, handler) {
+		handlers[name] = handlers[name] || [];
+		handlers[name].splice(handlers[name].indexOf(handler), 1);
+	});
+
+	this.dispatchEvent = function(evt) {
+		var handler = handlers[evt.type];
+		if(handler) {
+			handler.forEach(function(fn) {
+				fn.call(this, evt)
+			});
+		}
+	};
+
+	this.send = jasmine.createSpy('send');
+
+	var self = this;
+	this.close = jasmine.createSpy('close').and.callFake(function(){
+		sockets.splice(sockets.indexOf(self), 1);
+	});
+}
+
+
+// up yours, PhantomJS 1.9
+function createEvent(target, name, data) {
+	var evt = {
+		type: name,
+		target: target,
+	};
+
+	if(data) {
+		for(var key in data) {
+			if(data.hasOwnProperty(key)) {
+				evt[key] = data[key];
+			}
+		}
+	}
+
+	return evt;
+}
